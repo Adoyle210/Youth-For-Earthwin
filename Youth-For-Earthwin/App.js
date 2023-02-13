970d42cfc97e19a5662d2bf9f27fe989712c209f
Baleria Reyes <breyes815@gmail.com>
2023-02-13 13:38:05 -0800

    add text input for username and password

--- Youth-For-Earthwin/App.js
+++ Youth-For-Earthwin/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput style={{height: 40, textAlign: 'center', width: 200, borderColor: 'gray', borderWidth: 1}} placeholder="Username" />
+      <TextInput style={{height: 40, textAlign: 'center', width: 200, borderColor: 'gray', borderWidth: 1}} placeholder="Password" />
       <StatusBar style="auto" />
     </View>
   );
