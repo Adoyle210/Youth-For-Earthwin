3a0139b17f7a35e45ccb81fda3b48879ba21980f
Baleria Reyes <breyes815@gmail.com>
2023-02-13 13:49:46 -0800

    add random image for logo's potential location.

--- Youth-For-Earthwin/App.js
+++ Youth-For-Earthwin/App.js
@@ -1,9 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TextInput, 
+  Image 
+} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image
+        source={{uri: 'https://picsum.photos/200'}}
+        style={{width: 200, height: 200}}
+      />
       <TextInput style={{height: 40, textAlign: 'center', width: 200, borderColor: 'gray', borderWidth: 1}} placeholder="Username" />
       <TextInput style={{height: 40, textAlign: 'center', width: 200, borderColor: 'gray', borderWidth: 1}} placeholder="Password" />
       <StatusBar style="auto" />
