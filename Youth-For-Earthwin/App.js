8aa1fd59545c6701d87141a6429a00e6936774de
Baleria Reyes <breyes815@gmail.com>
2023-02-13 15:05:46 -0800

    add not functioning submit button.

--- Youth-For-Earthwin/App.js
+++ Youth-For-Earthwin/App.js
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { 
   StyleSheet, 
   Text, 
   View, 
   TextInput, 
-  Image 
+  Image,
+  // Font, 
+  TouchableOpacity 
 } from 'react-native';
+
+// TODO - Add fonts
+// const fetchFonts = () => {
+//   return Font.loadAsync({
+//     'Ecofont Vera Sans': require('./fonts/ecofont-vera-sans/ecofont_vera_sans_regular.ttf'),
+//   });
+// };
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 65}}>Earthwin </Text>
+      <Text style={{ fontSize: 65}}>Earthwin</Text>
       <Image
         source={{uri: 'https://picsum.photos/200'}}
         style={{width: 200, height: 200}}
@@ -18,6 +29,9 @@ export default function App() {
       <TextInput style={{height: 40, textAlign: 'center', width: 200, borderColor: 'gray', borderWidth: 1}} placeholder="Username" />
       <TextInput style={{height: 40, textAlign: 'center', width: 200, borderColor: 'gray', borderWidth: 1}} placeholder="Password" />
       <StatusBar style="auto" />
+      <TouchableOpacity style={{backgroundColor: 'green', width: 100, height: 40, alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={{color: 'white'}}>Log in</Text>
+      </TouchableOpacity>
     </View>
   );
 }
