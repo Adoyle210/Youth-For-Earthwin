265c74ca5224bd2c39184f3d6510a4632a0c77cb
Baleria Reyes <breyes815@gmail.com>
2023-02-13 16:18:15 -0800

    change shape of components

--- Youth-For-Earthwin/App.js
+++ Youth-For-Earthwin/App.js
@@ -24,12 +24,12 @@ export default function App() {
       <Text style={{ fontSize: 65, marginBottom: 10}}>Earthwin</Text>
       <Image
         source={{uri: 'https://picsum.photos/200'}}
-        style={{width: 200, height: 200, marginBottom: 5}}
+        style={{width: 200, height: 200, marginBottom: 5, borderRadius: 50}}
       />
-      <TextInput style={{height: 40, textAlign: 'center', width: 200, borderColor: 'gray', borderWidth: 1, marginBottom:.5}} placeholder="Username" />
-      <TextInput style={{height: 40, textAlign: 'center', width: 200, borderColor: 'gray', borderWidth: 1, marginBottom: 5}} placeholder="Password" />
+      <TextInput style={{height: 40, textAlign: 'center', width: 200, borderColor: 'gray', borderWidth: 1, marginBottom:.5, borderRadius: 5}} placeholder="Username" />
+      <TextInput style={{height: 40, textAlign: 'center', width: 200, borderColor: 'gray', borderWidth: 1, marginBottom: 5, borderRadius: 5}} placeholder="Password" />
       <StatusBar style="auto" />
-      <TouchableOpacity style={{backgroundColor: 'green', width: 100, height: 40, alignItems: 'center', justifyContent: 'center'}}>
+      <TouchableOpacity style={{backgroundColor: 'green', width: 100, height: 40, alignItems: 'center', justifyContent: 'center', borderRadius: 50}}>
         <Text style={{color: 'white'}}>Log in</Text>
       </TouchableOpacity>
     </View>
