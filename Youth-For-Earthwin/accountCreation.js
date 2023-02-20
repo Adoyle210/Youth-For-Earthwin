12f7826efd1ffc6117c3830bd0a52850a6519777
Baleria Reyes <breyes815@gmail.com>
2023-02-20 11:42:26 -0800

    Add text input boxes for AccountCreation

--- Youth-For-Earthwin/accountCreation.js
+++ Youth-For-Earthwin/accountCreation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 function AccountCreation() {
@@ -7,9 +7,20 @@ function AccountCreation() {
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Account Creation</Text>
+      <Text>Create an account</Text>
       <Button
-        title="Go to Login"
+        title="Back to Login"
+        onPress={() => navigation.navigate('Login')}
+      />
+      <TextInput style={{height: 40, textAlign: 'center', width: 350, borderColor: 'gray', borderWidth: 1, marginBottom: 10, borderRadius: 5}} placeholder="First Name" />
+      <TextInput style={{height: 40, textAlign: 'center', width: 350, borderColor: 'gray', borderWidth: 1, marginBottom:10, borderRadius: 5}} placeholder="Last Name" />
+      <TextInput style={{height: 40, textAlign: 'center', width: 350, borderColor: 'gray', borderWidth: 1, marginBottom:10, borderRadius: 5}} placeholder="Username" />
+      <TextInput style={{height: 40, textAlign: 'center', width: 350, borderColor: 'gray', borderWidth: 1, marginBottom: 10, borderRadius: 5}} placeholder="Password" secureTextEntry={true}/>
+      <TextInput style={{height: 40, textAlign: 'center', width: 350, borderColor: 'gray', borderWidth: 1, marginBottom: 10, borderRadius: 5}} placeholder="Confirm Password" secureTextEntry={true}/>
+      <TextInput style={{height: 40, textAlign: 'center', width: 350, borderColor: 'gray', borderWidth: 1, marginBottom: 10, borderRadius: 5}} placeholder="Email" />
+      <TextInput style={{height: 40, textAlign: 'center', width: 350, borderColor: 'gray', borderWidth: 1, marginBottom: 10, borderRadius: 5}} placeholder="Phone Number" />
+      <Button
+        title="Create Account"
         onPress={() => navigation.navigate('Login')}
       />
     </View>
