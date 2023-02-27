865ca6c766ce6c2e1c405be54e91b9911fa7c650
Baleria Reyes <breyes815@gmail.com>
2023-02-27 14:19:47 -0800

    Add submitButton, header, subheader, and title styles in styles.js

--- Youth-For-Earthwin/styles.js
+++ Youth-For-Earthwin/styles.js
@@ -25,12 +25,39 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     borderRadius: 5
   },
+  submitButton: {
+    backgroundColor: 'green',
+    width: 100,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+    marginBottom: 10
+  },
   backButton: {
     width: 100,
     position: 'absolute',
     top: 10,
-    left: 10
-  }
+    left: 10,
+  },
+  header: {
+    backgroundColor: 'green',
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 20,
+    width: '100%',
+  },
+  subheader: {
+    fontSize: 15,
+    marginBottom: 10,
+  },
+  title: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
 })
 
 export default styles;
