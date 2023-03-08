d4d149ca9f56a58d73099211fce9f93b10524850
Baleria Reyes <breyes815@gmail.com>
2023-03-08 13:47:09 -0800

    Add ProjectOverview.js

---
+++ Youth-For-Earthwin/ProjectOverview.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from "react-native";
+import styles from "./styles.js";
+
+class ProjectOverview extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+}
+
