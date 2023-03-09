6bfdad58c64f6fa9f1d3f3020dac59fdb9eddb9b
Baleria Reyes <breyes815@gmail.com>
2023-03-08 17:48:56 -0800

    Add render function and new project handler

--- Youth-For-Earthwin/ProjectOverview.js
+++ Youth-For-Earthwin/ProjectOverview.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
-import { StyleSheet, Text, View, TouchableOpacity, FlatList } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Alert } from "react-native";
 import styles from "./styles.js";
 
 class ProjectOverview extends React.Component {
@@ -11,8 +11,36 @@ class ProjectOverview extends React.Component {
     }
   }
 
-  
+  handleNewProject = () => {
+    Alert.prompt(
+      "New Project",
+      "Pick a name for your new project",
+      [
+        {
+          text: "Cancel",
+          style: "cancel"
+        },
+        {
+          text: "OK",
+          onPress: (text) => {
+            this.setState({
+              projects: [...this.state.projects, {key: text}]
+            });
+          }
+        }
+      ]
+    )
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Project Overview</Text>
+        </View>
+      </View>
+    )
+  }
 }
 
-
-
+export default ProjectOverview;
