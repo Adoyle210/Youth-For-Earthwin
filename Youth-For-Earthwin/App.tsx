b85ea55755c47bb67296254092c3c52406749d68
Baleria Reyes <breyes815@gmail.com>
2023-04-19 17:08:50 -0700

    Adjusted to display LoginScreen

--- Youth-For-Earthwin/App.tsx
+++ Youth-For-Earthwin/App.tsx
@@ -1,28 +1,40 @@
-import 'react-native-url-polyfill/auto'
-import { useState, useEffect } from 'react'
-import { supabase } from './config/supabase'
-import Auth from './components/Auth'
-import Account from './components/Account'
-import { View } from 'react-native'
-import { Session } from '@supabase/supabase-js'
+import 'react-native-url-polyfill/auto';
+import { useState, useEffect } from 'react';
+import { supabase } from './config/supabase';
+import LoginScreen from './screens/LoginScreen';
+import Account from './components/Account';
+import { SafeAreaView, StyleSheet } from 'react-native'; // Import StyleSheet
+import { Session } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
 
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null)
+  const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+      setSession(session);
+    });
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+      setSession(session);
+    });
+  }, []);
 
   return (
-    <View>
-      {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      {session && session.user ? (
+        <Account key={session.user.id} session={session} />
+      ) : (
+        <LoginScreen />
+      )}
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
