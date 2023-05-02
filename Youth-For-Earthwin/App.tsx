11e296b3151c4c9945a106318ae8e4bbf7c053f2
Baleria Reyes <breyes815@gmail.com>
2023-05-02 15:28:33 -0700

    Adjustments for AppNavigator

--- Youth-For-Earthwin/App.tsx
+++ Youth-For-Earthwin/App.tsx
@@ -1,25 +1,9 @@
 import 'react-native-url-polyfill/auto';
 import { useState, useEffect } from 'react';
 import { supabase } from './config/supabase';
-import { SafeAreaView, StyleSheet } from 'react-native'; // Import StyleSheet
+import { SafeAreaView, StyleSheet } from 'react-native';
 import { Session } from '@supabase/supabase-js';
-import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Account from './components/Account';
-
-//screens
-import LoginScreen from './screens/LoginScreen';
-import HomeScreen from './screens/HomeScreen';
-import DetailsScreen from './screens/DetailsScreen';
-import ProjectApprovalScreen from './screens/ProjectApprovalScreen';
-import ProjectPitch from './screens/ProjectPitch';
-import ProjectOverview from './ProjectOverview';
-import ParentProfile from './screens/ParentProfile';
-import StudentProfile from './screens/StudentProfile';
-import ForgotPassword from './ForgotPassword';
-
-const Stack = createNativeStackNavigator();
+import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
   const [session, setSession] = useState<Session | null>(null);
@@ -35,29 +19,11 @@ export default function App() {
   }, []);
 
   return (
-    <NavigationContainer>
-      <SafeAreaView style={styles.container}>
-        {session && session.user ? (
-          <Account key={session.user.id} session={session} />
-        ) : (
-          <LoginScreen />
-        )}
-        {/* <Stack.Navigator initialRouteName="Home">     
-          <Stack.Screen name="Home" component={HomeScreen}/>
-          <Stack.Screen name="Details" component={DetailsScreen}/>
-          <Stack.Screen name="Project Approval" component={ProjectApprovalScreen}/>
-          <Stack.Screen name="Project Pitch" component={ProjectPitch}/>
-          <Stack.Screen name="Parent Profile" component={ParentProfile} />
-          <Stack.Screen name="Student Profile" component={StudentProfile} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Forgot Password" component={ForgotPassword} />
-        </Stack.Navigator> */}
-      </SafeAreaView>
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <AppNavigator session={session} />
+    </SafeAreaView>
   );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
