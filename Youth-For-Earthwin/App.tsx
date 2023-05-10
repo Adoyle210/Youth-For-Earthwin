957b38ff2b67317dffc3c1a24e7d609cc4afc996
Baleria Reyes <breyes815@gmail.com>
2023-05-09 17:29:24 -0700

    Edit to return only AppNavigator

--- Youth-For-Earthwin/App.tsx
+++ Youth-For-Earthwin/App.tsx
@@ -1,32 +1,8 @@
-import 'react-native-url-polyfill/auto';
-import { useState, useEffect } from 'react';
-import { supabase } from './config/supabase';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import { Session } from '@supabase/supabase-js';
+import React from 'react';
 import AppNavigator from './navigation/AppNavigator';
 
-export default function App() {
-  const [session, setSession] = useState<Session | null>(null);
+const App = () => {
+  return <AppNavigator />;
+};
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <AppNavigator session={session} />
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  }
-});
+export default App;
