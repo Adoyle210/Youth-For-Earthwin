36ff50ff9737440d727accbfcf33c6e50bf4a656
Baleria Reyes <breyes815@gmail.com>
2023-05-11 22:34:03 -0700

    add calculator button

--- Youth-For-Earthwin/screens/HomeScreen.js
+++ Youth-For-Earthwin/screens/HomeScreen.js
@@ -3,7 +3,6 @@ import {View, Text, StyleSheet, TouchableOpacity, Image} from "react-native";
 import Menu from '../components/Menu';
 import Profile from "../components/Profile";
 
-
 const HomeScreen = ({navigation}) => {
     return (
         <View style={styles.mainContainer}>
@@ -14,34 +13,38 @@ const HomeScreen = ({navigation}) => {
                 <Text style={styles.mainHeader}>Home Screen</Text>
                 <View style={styles.leftContainer}>
                     <TouchableOpacity style={styles.buttonStyle} onPress={() => navigation.navigate("Gather Group")}>
-                    <Image source={require('../assets/group.png')} style={styles.image}/>
+                        <Image source={require('../assets/group.png')} style={styles.image}/>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.rightContainer}>
                     <TouchableOpacity style={styles.buttonStyle} onPress={() => navigation.navigate("Project Pitch")}>
-                    <Image source={require('../assets/project.png')} style={styles.image}/>
+                        <Image source={require('../assets/project.png')} style={styles.image}/>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.leftContainer}>
                     <TouchableOpacity style={styles.buttonStyle} onPress={() => navigation.navigate("Funding")}>
-                    <Image source={require('../assets/funds.png')} style={styles.image}/>
+                        <Image source={require('../assets/funds.png')} style={styles.image}/>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.rightContainer}>
                     <TouchableOpacity style={styles.buttonStyle} onPress={() => navigation.navigate("Final Step")}>
-                    <Image source={require('../assets/flag.png')} style={styles.image}/>
+                        <Image source={require('../assets/flag.png')} style={styles.image}/>
                     </TouchableOpacity>
                 </View>
-
+                <View style={styles.centerContainer}>
+                    <TouchableOpacity style={styles.buttonStyle} onPress={() => navigation.navigate("Idling2")}>
+                        <Image source={require('../assets/calculator.png')} style={styles.newImage}/>
+                    </TouchableOpacity>
+                </View>
             </View>
             <View style={styles.MenuStyle}>
-                    <View style={styles.lineStyle}> </View>
-                    <Menu/>
-                    <View style={[styles.lineStyle, {
-                        marginVerical: 20,
-                    }]}> </View>
+                <View style={styles.lineStyle}> </View>
+                <Menu/>
+                <View style={[styles.lineStyle, {
+                    marginVerical: 20,
+                }]}> </View>
             </View>
-         </View>
+        </View>
     );
 };
 
@@ -65,11 +68,6 @@ const styles = StyleSheet.create({
         color: "#344055",
         textTransform: "uppercase",
         fontFamily: "Nunito_600SemiBold",
-        // fontWeight: 'bold',
-    },
-    textstyle: {
-        fontSize: 20,
-        color: "#344055",
     },
     lineStyle: {
         marginBottom: 10,
@@ -89,12 +87,30 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
     },
+    centerContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
     image: {
         width: 100,
         height: 100,
-        borderRadius: 200/2
-    }
+        borderRadius: 200/2,
+    },
+    newImage: {
+        width: 100,
+        height: 100,
+        resizeMode: 'contain'
+    },
+    buttonStyle: {
+        padding: 10,
+        margin: 5,
+        height: 100,
+        width: 100,
+    },
+    MenuStyle: {
+        alignSelf: 'flex-end',
+    },
 });
 
 export default HomeScreen;
-  
+
