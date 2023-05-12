a15322922f5233f261d873e928795f53f549f62c
JudeWi <86169929+JudeWi@users.noreply.github.com>
2023-05-11 21:28:22 -0700

    Update IdlingScreen1
    Fixed w/ Randen's edits.

--- Youth-For-Earthwin/screens/IdlingProj/Idling1Screen.js
+++ Youth-For-Earthwin/screens/IdlingProj/Idling1Screen.js
@@ -12,7 +12,7 @@ function Idling1Screen() {
             <Text style={styles.TextStyle}>
                 EarthWin projects rely on a simple idea: 
                 <Text style={styles.TextStyleBold}> Local efforts result in global change. </Text>
-                With that mindset, a small group of students in [YEAR] 
+                With that mindset, a small group of students in 2021 
                 set out to make a small change at their local school in Bend, Oregon.
                 {'\n\n'}
                 They realized that parents, idling in the parking lot while waiting 
@@ -37,7 +37,7 @@ function Idling1Screen() {
         </View>
 
         <View>
-            <View style={styles.FiveBox1}>
+            {/* <View style={styles.FiveBox1}>
                 <Text style={styles.TextStyle}> 
                     <Text style={styles.TextStyleBold}>Gather a group:</Text>
                         {'\n\n'}
@@ -48,8 +48,13 @@ function Idling1Screen() {
                         environmentally conscious groups to find people who may be 
                         interested in joining. 
                 </Text>
-            </View>
-            <View style={styles.FiveBox2}>
+                
+                <TouchableOpacity style={styles.ButtonStyle} onPress={() => navigation.navigate("Idling2")}>
+                <Text style={styles.submitButtonText}> Learn More </Text>
+                </TouchableOpacity>
+            </View> */}
+            
+            <View style={styles.FiveBox1}>
                 <Text style={styles.TextStyle}>
                     <Text style={styles.TextStyleBold}>Decide on your problem and goal:</Text>
                         {'\n\n'}
@@ -60,8 +65,40 @@ function Idling1Screen() {
                         to install "No Idling" signs, along with changing the schools rules regarding
                         idling vehicles in the parking lot.
                 </Text>
+                <TouchableOpacity style={styles.ButtonStyle} onPress={() => navigation.navigate("Idling2")}>
+                    <Text style={styles.submitButtonText}> Learn More </Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.FiveBox2}>
+                <Text style={styles.TextStyle}>
+                    <Text style={styles.TextStyleBold}>Audit and Measure:</Text>
+                        {'\n\n'}
+                        Having evidence to back the necessity of your plan is key. Learning what to measure,
+                        and how to measure it, is an important part of the EarthWin process.
+                        {'\n\n'}
+                        The ideas that we aim to instill in students aren't just a strong sense of environmental
+                        values, but also a strong ability to reason, deduce, measure, and analyze on their own.  
+                </Text>
+                <TouchableOpacity style={styles.ButtonStyle} onPress={() => navigation.navigate("Idling2")}>
+                    <Text style={styles.submitButtonText}> Learn More </Text>
+                </TouchableOpacity>
             </View>
             <View style={styles.FiveBox3}>
+                <Text style={styles.TextStyle}>
+                    <Text style={styles.TextStyleBold}>Develop a plan:</Text>
+                        {'\n\n'}
+                        Now that you've determined the impact of your problem, it's time to come up
+                        with a plan as to how to solve it.
+                        {'\n\n'}
+                        Problem solving, brainstorming, compromise and cooperation are all elements that
+                        are key throughout your life. Learning how to build up a plan that comprehensively
+                        addresses your issue and can be implemented will help teach you all of these ideas.
+                </Text>
+                <TouchableOpacity style={styles.ButtonStyle} onPress={() => navigation.navigate("Idling2")}>
+                    <Text style={styles.submitButtonText}> Learn More </Text>
+                </TouchableOpacity>    
+            </View>
+            <View style={styles.FiveBox4}>
                 <Text style={styles.TextStyle}>
                     <Text style={styles.TextStyleBold}>Connect with people who can help:</Text>
                         {'\n\n'}
@@ -71,9 +108,12 @@ function Idling1Screen() {
                         EarthWin always emphasizes that students go out and find help, first in the form
                         of an advisor who works with their school, then via community members who can help
                         get the word out, fundraise, or volunteer.
-                </Text>    
+                </Text>
+                <TouchableOpacity style={styles.ButtonStyle} onPress={() => navigation.navigate("Idling2")}>
+                    <Text style={styles.submitButtonText}> Learn More </Text>
+                </TouchableOpacity>    
             </View>
-            <View style={styles.FiveBox4}>
+            <View style={styles.FiveBox5}>
                 <Text style={styles.TextStyle}>
                     <Text style={styles.TextStyleBold}>Spread the word:</Text>
                         {'\n\n'}
@@ -84,17 +124,9 @@ function Idling1Screen() {
                         to seek help from EarthWin and their advisor to help create materials such as signs,
                         flyers, petitions, and even social media posts via EarthWin approved apps.
                 </Text>
-            </View>
-            <View style={styles.FiveBox5}>
-                <Text style={styles.TextStyle}>
-                    <Text style={styles.TextStyleBold}>Audit and Measure:</Text>
-                        {'\n\n'}
-                        Having evidence to back the necessity of your plan is key. Learning what to measure,
-                        and how to measure it, is an important part of the EarthWin process.
-                        {'\n\n'}
-                        The ideas that we aim to instill in students aren't just a strong sense of environmental
-                        values, but also a strong ability to reason, deduce, measure, and analyze on their own.  
-                </Text>
+                <TouchableOpacity style={styles.ButtonStyle} onPress={() => navigation.navigate("Idling2")}>
+                    <Text style={styles.submitButtonText}> Learn More </Text>
+                </TouchableOpacity>
             </View>
         </View>
 
@@ -105,11 +137,11 @@ function Idling1Screen() {
             <Text style={styles.TextStyle}>
                 Our students in Bend followed these five steps, and after taking measurements spanning approximately 
                 a week, were able to present a full plan to their school district to solve the idling issues at their school.
-
+                {'\n\n'}
                 Click below to see how the students of Bend measured the impact of the idling vehicles on their school!
-                </Text>
-            <TouchableOpacity style={styles.buttonStyle} onPress={() => navigation.navigate("Idling2")}>
-                <Text> Continue </Text>
+            </Text>
+            <TouchableOpacity style={styles.ButtonStyle} onPress={() => navigation.navigate("Idling2")}>
+                <Text style={styles.submitButtonText}> Continue </Text>
             </TouchableOpacity>
         </View>
         
@@ -152,6 +184,22 @@ const styles = StyleSheet.create({
     },
     TextStyleBold: {
         fontWeight: 'bold',   
+    },
+    ButtonStyle: {
+        alignSelf: 'center',
+        backgroundColor: 'green',
+        width: 100,
+        height: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 50,
+        marginBottom: 10,
+        marginTop: 5,
+    },
+    submitButtonText: {
+        color: 'white',
+        fontSize: 15,
+        fontWeight: 'bold',
     },
     LeftBox: {
         marginRight: "33%",
